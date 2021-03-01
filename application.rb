f390d13b698c876760c0a77b69149f5cbb17dbeb
ak-matsu <sandbox.matsu@gmail.com>
2021-03-01 21:44:00 +0900

    飲み物のデータを生成

--- application.rb
+++ application.rb
@@ -1,4 +1,18 @@
+#自販機で必要な3つのクラスを定義
 class Drink
+  def initialize(name,fee)
+    @name = name
+    @fee = fee
+  end
+
+  def name
+    @name
+  end
+
+  def fee
+    @fee
+    
+  end
   
 end
 
@@ -9,3 +23,15 @@ end
 class User
   
 end
+
+puts "商品を用意してください"
+drings  = []
+3.times do |i|
+  puts "商品名を入力してください"
+  drink_name  = gets.chomp
+  puts "金額を入力してください"
+  drink_fee = gets.to_i
+  drinks << Drink.new(drink_name,drink_fee)
+end
+
+puts drinks
