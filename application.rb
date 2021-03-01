0e4f699ca6bd7288e92e10f7fa0e5ba668eb0b42
ak-matsu <sandbox.matsu@gmail.com>
2021-03-01 22:35:19 +0900

    コメントアウト

--- application.rb
+++ application.rb
@@ -21,6 +21,7 @@ end
 #投入金額からお釣りを計算する
 class VendingMachine
   def initialize(drinks)
+    #@drinksには3つのドリンク情報が含まれる
     @drinks = drinks
   end
 
@@ -28,9 +29,14 @@ class VendingMachine
     @drinks
   end
 
+  #show_drinksメソッドで一覧表示にする。
   def show_drinks
     puts  "いらっしゃいませ、以下の商品を販売しています。"
     i = 0
+    #self.はクラスメソッド
+    #self.drinksの”self”は生成したインスタンス、
+    #"drinks"は直前で定義したインスタンスメソッドであり、@drinksの情報を意味する。
+    #self.drinksをしないと、同じクラス内（VendingMachine）で生成した@drinksを使うことができないから。
     self.drinks.each  do  |drink|
       puts "【#{i}】#{drink.name}:  #{drink.fee}円"
       i += 1
@@ -54,5 +60,8 @@ drinks  = []
   drinks << Drink.new(drink_name,drink_fee)
 end
 
+#VendingMachineクラスのインスタンスを生成。drinksへ3つの商品が格納され
+#initializeメソッドへで生成した@drinksへ代入される。
 vending_machine = VendingMachine.new(drinks)
+#生成したインスタンスに対してshow_drinksメソッドを適用させる。
 vending_machine.show_drinks
