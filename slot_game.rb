913ae2aee974d5a9739e6c3b91508c8eceef8c4c
ak-matsu <sandbox.matsu@gmail.com>
2021-03-02 00:22:26 +0900

    resultの打ち間違い

--- slot_game.rb
+++ slot_game.rb
@@ -1,7 +1,7 @@
 class SlotGame
   def play_slot
     result = []
-    #3回繰り返す
+    #3回繰り返す 
     3.times do
       #rand関数でランダムに3回値を生成して配列resultへ代入。
       result << rand(0..9)
@@ -10,7 +10,7 @@ class SlotGame
     puts "スロットゲームの結果は#{result.join}です！"
     
     #3つの数字が揃っているか判断させている。
-    if result[0] == resulut[1] && resulut[0] == result[2]
+    if result[0] == result[1] && result[0] == result[2]
       puts "おめでとうございます！"
     else
       puts  "残念でした〜"
